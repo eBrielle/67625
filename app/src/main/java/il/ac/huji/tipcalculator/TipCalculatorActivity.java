2ea04bcd4347e7cea40a0bb561d4b9286d467a4d
eBrielle <esther.feldblum@mail.huji.ac.il>
2016-03-05 03:05:40 +0200

    Initial commit

---
+++ app/src/main/java/il/ac/huji/tipcalculator/TipCalculatorActivity.java
@@ -0,0 +1,13 @@
+package il.ac.huji.tipcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TipCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_calculator);
+    }
+}
